062f971fe33082160c5be085d000a84165a98fcd
Ana.Sousa <anasousa314@outlook.com>
2022-03-01 16:50:50 -0300

    feat: create Navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
-
+import React from "react";
+import styles from "../styles/Navbar.module.css";
+import Image from "next/image";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image
+            src="/img/telephone.png"
+            width={32}
+            height={32}
+            alt="telephone"
+          />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>012 333 156</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>
+            Homepage
+          </li>
 
-export default Navbar
+          <li className={styles.listItem}>
+            Product
+          </li>
+
+          <li className={styles.listItem}>
+            Menu
+          </li>
+
+          <Image src="/img/logo.png" alt="Logo" width={160} height={69}
+          />
+
+          <li className={styles.listItem}>
+            Events
+          </li>
+
+          <li className={styles.listItem}>
+            Blog
+          </li>
+
+          <li className={styles.listItem}>
+            Contact
+          </li>
+        </ul>  
+      </div>
+      <div className={styles.item}>
+          <div className={styles.cart}>
+            <Image src="/img/cart.png" alt="Logo" width=  {30} height={30}/>
+            <div className={styles.counter}>2</div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Navbar/>
+ 
       <Head>
         <title>Pizza Restaurant</title>
         <meta name="description" content="Best Pizza shop in town" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       homepage
-      <Footer/>
 
     </div>
   )
